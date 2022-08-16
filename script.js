4948dd740a2e99938e993a13d619cfcfb2687fe0
umeshgt <79749810+Umeshgt@users.noreply.github.com>
2022-08-16 19:07:55 +0530

    finale-part-1

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 canvas = document.getElementById("canvas1")
 ctx = canvas.getContext("2d")
 console.log(ctx)
-canvas.width = 800
-canvas.height = 700
+CANVAS_WIDTH = canvas.width = 800
+CANVAS_HEIGHT = canvas.height = 700
 let gamespeed = 15
 
 let x = 0
 let x2 = 0
 
-BImage1 = new Image()
-BImage1.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
-BImage2 = new Image()
-BImage2.src = "C:\projects\parallaxBackground\backgroundLayers\layer-2.png"
-BImage3 = new Image()
-BImage3.src = "C:\projects\parallaxBackground\backgroundLayers\layer-3.png"
-BImage4 = new Image()
-BImage4.src = "C:\projects\parallaxBackground\backgroundLayers\layer-4.png"
-BImage5 = new Image()
-BImage5.src = "C:\projects\parallaxBackground\backgroundLayers\layer-5.png"
+const BImage1 = new Image()
+BImage1.src = "layer-1.png"
+const BImage2 = new Image()
+BImage2.src = "layer-2.png"
+const BImage3 = new Image()
+BImage3.src = "layer-3.png"
+const BImage4 = new Image()
+BImage4.src = "layer-4.png"
+const BImage5 = new Image()
+BImage5.src = "layer-5.png"
 
 
 function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height )
-    ctx.drawImage(BImage5, x,0)
-    ctx.drawImage(BImage5, x2,0)
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT )
+    ctx.drawImage(BImage3, x,0)
+    ctx.drawImage(BImage3, x2,0)
     if(x < -2400) x = 2400 + x2 - gamespeed
     else x = x - gamespeed  // keep going left 
     if(x2 < -2400) x2 = 2400 + x - gamespeed
