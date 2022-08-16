5255ab60d2f2281005778b77a9921a60ed14b028
umeshgt <79749810+Umeshgt@users.noreply.github.com>
2022-08-16 18:50:50 +0530

    part-1

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ ctx = canvas.getContext("2d")
 console.log(ctx)
 canvas.width = 800
 canvas.height = 700
+let gamespeed = 15
 
-x=0
-BImage
+let x = 0
+let x2 = 0
+
+BImage1 = new Image()
+BImage1.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
+BImage2 = new Image()
+BImage2.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
+BImage3 = new Image()
+BImage3.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
+BImage4 = new Image()
+BImage4.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
+BImage5 = new Image()
+BImage5.src = "C:\projects\parallaxBackground\backgroundLayers\layer-1.png"
+
 
 function animate(){
-
+    ctx.clearRect(0,0, canvas.width, canvas.height )
+    ctx.drawImage(BImage5, x,0)
+    ctx.drawImage(BImage5, x2,0)
+    if(x < -2400) x = 2400 + x2 - gamespeed
+    else x = x - gamespeed  // keep going left 
+    if(x2 < -2400) x2 = 2400 + x - gamespeed
+    else x2 = x2 - gamespeed  // keep going left 
 
     requestAnimationFrame(animate)
 }
