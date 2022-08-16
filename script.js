733bbba537819ecab5aacecd367d56621deca8b9
umeshgt <79749810+Umeshgt@users.noreply.github.com>
2022-08-16 12:52:50 +0530

    layers

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ ctx = canvas.getContext("2d")
 console.log(ctx)
 canvas.width = 800
 canvas.height = 700
+
+x=0
+BImage
+
+function animate(){
+
+
+    requestAnimationFrame(animate)
+}
+
+animate();
